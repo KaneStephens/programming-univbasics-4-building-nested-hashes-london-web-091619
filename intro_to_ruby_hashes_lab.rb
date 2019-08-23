97407fbaa8f5e0659ee1ee0abb49a84cba4166c2
Kane Pangalia Stephens <kanepstephens@gmail.com>
2019-08-23 10:12:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,9 +23,9 @@ def monopoly_with_third_tier
         :four_pieces_owned => 200},
       :names => {
         :reading_railroad => {},
-        :pennsylvania_railroad => {"mortgage_value" => "$200"},
-        :b_and_o_railroad => {"mortgage_value" => "$400"},
-        :shortline_railroad => {"mortgage_value" => "$800"}
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
+        :shortline_railroad => {}
       },
     } 
   }
