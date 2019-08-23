f9630fe086f600ed2f813d56e73434cdd9475962
Kane Pangalia Stephens <kanepstephens@gmail.com>
2019-08-23 09:48:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ end
 
 def monopoly_with_third_tier
   local_base_hash = {
-    :railroads => {:pieces => 4, :rent_in_dollars => }
+    :railroads => {:pieces => 4, :rent_in_dollars => 120 }
   }
   local_base_hash
 end
