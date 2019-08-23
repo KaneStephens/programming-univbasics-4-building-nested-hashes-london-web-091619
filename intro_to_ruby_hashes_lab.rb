2cd6da68f614e8fca64594818746a8039925a321
Kane Pangalia Stephens <kanepstephens@gmail.com>
2019-08-23 09:57:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4, 
       :rent_in_dollars => {
-      :key1 => "value1",
+      :one_piece_owned => 25,
       :key2 => "value2",
       :key3 => "value3",
       :key4 => "value4"},
